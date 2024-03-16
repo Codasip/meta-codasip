d6035279b84ed0e1b4cb7ac069641bbdbb18bbd0
Stuart Menefy <stuart.menefy@codasip.com>
2024-03-16 13:59:47 +0000

    hobgoblin_yocto_setup.sh: Switch from sysvinit to systemd
    This was part of the R1 release spec, and should be faster.

--- hobgoblin_yocto_setup.sh
+++ hobgoblin_yocto_setup.sh
@@ -55,7 +55,7 @@ USER_CLASSES ?= "buildstats buildhistory buildstats-summary"
 #require conf/distro/include/security_flags.inc
 
 INHERIT += "uninative"
-DISTRO_FEATURES = "ipv4 sysvinit"
+DISTRO_FEATURES = "ipv4 systemd usrmerge"
 IMAGE_FSTYPES="ext4 wic"
 TOOLCHAIN = "clang"
 EOF
